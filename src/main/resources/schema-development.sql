0e2001cc9dbd698809714f8dd6ec1d3e633cd615
mvelusce <marcoveluscek@gmail.com>
2018-01-06 13:28:57 +0100

    Config fix to compile on Windows

--- src/main/resources/schema-development.sql
+++ src/main/resources/schema-development.sql
@@ -1,1 +1,24 @@
-schema-prod.sql
+
+DROP TABLE IF EXISTS recipes, ingredients, recipe_ingredients;
+
+CREATE TABLE recipes (
+	id 		        bigint auto_increment primary key,
+    name            varchar(255) not null,
+    type            varchar(255),
+    description     text,
+    instructions    text,
+    notes           text
+);
+
+CREATE TABLE ingredients (
+    id              bigint auto_increment primary key,
+    name            varchar(255) not null,
+    description     text
+);
+
+CREATE TABLE recipe_ingredients (
+    id              bigint auto_increment primary key,
+    recipe          bigint,
+    ingredient      bigint,
+    quantity        varchar(40)
+);
