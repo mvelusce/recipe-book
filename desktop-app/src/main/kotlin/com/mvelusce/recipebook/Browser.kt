33f2f2eefe859098e17909941bbc740915807201
Marco Veluscek <marco.veluscek@itecons.it>
2018-01-17 14:09:33 +0100

    Loading web app in JavaFX application

--- desktop-app/src/main/kotlin/com/mvelusce/recipebook/Browser.kt
+++ desktop-app/src/main/kotlin/com/mvelusce/recipebook/Browser.kt
@@ -1,2 +1,45 @@
 package com.mvelusce.recipebook
 
+import javafx.geometry.HPos
+import javafx.geometry.VPos
+import javafx.scene.Node
+import javafx.scene.layout.HBox
+import javafx.scene.layout.Priority
+import javafx.scene.layout.Region
+import javafx.scene.web.WebView
+
+class Browser : Region() {
+
+    val browser = WebView()
+    val webEngine = browser.engine
+
+    init {
+        //apply the styles
+        styleClass.add("browser")
+        // load the web page
+        webEngine.load("http://localhost:9000/recipe-book/")
+        //add the web view to the scene
+        children.add(browser)
+
+    }
+
+    private fun createSpacer(): Node {
+        val spacer = Region()
+        HBox.setHgrow(spacer, Priority.ALWAYS)
+        return spacer
+    }
+
+    override fun layoutChildren() {
+        val w = width
+        val h = height
+        layoutInArea(browser, 0.0, 0.0, w, h, 0.0, HPos.CENTER, VPos.CENTER)
+    }
+
+    override fun computePrefWidth(height: Double): Double {
+        return 750.0
+    }
+
+    override fun computePrefHeight(width: Double): Double {
+        return 500.0
+    }
+}
--- desktop-app/src/main/kotlin/com/mvelusce/recipebook/MainFrame.kt
+++ desktop-app/src/main/kotlin/com/mvelusce/recipebook/MainFrame.kt
@@ -1,2 +1,62 @@
 package com.mvelusce.recipebook
 
+import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.paint.Color
+import javafx.stage.Stage
+
+class MainFrame : Application() {
+
+    private var scene: Scene? = null
+
+    override fun start(stage: Stage) {
+        // create the scene
+        stage.title = "Desktop Application"
+        scene = Scene(Browser(), 750.0, 500.0, Color.web("#666970"))
+        stage.scene = scene
+        //scene.getStylesheets().add("webviewsample/BrowserToolbar.css");
+        stage.show()
+    }
+
+    override fun stop() {
+        if (p != null && p!!.isAlive) {
+            p!!.destroy()
+        }
+    }
+
+    companion object {
+
+        private var p: Process? = null
+
+        @Throws(InterruptedException::class)
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+            runWebapp()
+
+            Thread.sleep(20000)
+
+            launch(MainFrame::class.java, *args)
+        }
+
+        private fun runWebapp() {
+            try {
+                val command = "java -jar ./webapp/web-app-0.0.1-SNAPSHOT.war"
+
+                val line: String
+                p = Runtime.getRuntime().exec(command)
+
+                /*BufferedReader input =
+                    new BufferedReader
+                            (new InputStreamReader(p.getInputStream()));
+            while ((line = input.readLine()) != null) {
+                System.out.println(line);
+            }
+            input.close();*/
+            } catch (err: Exception) {
+                err.printStackTrace()
+            }
+
+        }
+    }
+}
