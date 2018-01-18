3043cf3fc6895359fe16e6d468092eafb7a33232
Marco Veluscek <marco.veluscek@itecons.it>
2018-01-18 17:55:52 +0100

    Desktop app depends on web app and add war to jar

--- desktop-app/src/main/kotlin/com/mvelusce/recipebook/config/Config.kt
+++ desktop-app/src/main/kotlin/com/mvelusce/recipebook/config/Config.kt
@@ -4,5 +4,5 @@ object Config {
 
     val webAppUrl = "http://localhost:9000/recipe-book/"
 
-    val webAppExecCommand = "java -jar ./desktop-app/webapp/web-app-0.0.1-SNAPSHOT.war"
+    val webAppExecCommand = "java -jar ./web-app-0.0.1-SNAPSHOT.war"
 }
