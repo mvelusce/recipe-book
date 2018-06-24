042473663cf8f40ee2efaa359dc163aecb70b9eb
mvelusce <marcoveluscek@gmail.com>
2018-06-24 17:14:57 +0200

    Update push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -5,16 +5,16 @@ setup_git() {
   git config --global user.name "mvelusce"
 }
 
-commit_website_files() {
+commit_files() {
   git checkout master
   git add .
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git push "https://${GH_TOKEN}@github.com:mvelusce/recipe-book.git" master
+  git push -f "https://${GH_TOKEN}@github.com:mvelusce/recipe-book.git" master
 }
 
 setup_git
-commit_website_files
+commit_files
 upload_files
