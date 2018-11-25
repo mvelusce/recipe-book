a925a98e82e6590e94d73beab514f695aaea3aa1
Marco Veluscek <marcoveluscek@gmail.com>
2018-11-25 17:11:32 +0100

    Add fast-csv module to work with CSV files

--- src/app/services/import-export/import-export.service.ts
+++ src/app/services/import-export/import-export.service.ts
@@ -1,14 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { fromPath } from 'fast-csv'
 
 import { NedbDaoService } from '../dao/nedb-dao.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ImportExportService {
+export class ImportExportService implements OnInit {
 
   constructor(private nedbDaoService: NedbDaoService) { }
+
+  ngOnInit() {
+    this.importRecipes("data/ricette.csv");
+  }
 
   exportRecipes(): void {
     this.nedbDaoService.getRecipes().subscribe(recipes => {
@@ -18,7 +23,18 @@ export class ImportExportService {
       });
   }
 
-  importRecipes(): void {
+  importRecipes(path: string): void {
+    const stream = fromPath(path, {
+        headers: true,
+        delimiter: ";"
+    })
+    stream.on("data", function(data){
+        console.log(data);
+    })
+    .on("end", function(){
+        console.log("done");
+    });
+    //fs.createReadStream(path);
     // TODO read recipes and insert in DB
     /* this.nedbDaoService.addRecipe({ name } as Recipe)
       .subscribe(recipe => {
